5f213433e3665e396dd13afb456c66fac5117b5c
Ravi kumar <ravi720rathore@gmail.com>
2026-05-10 23:46:40 +0530

    Add files via upload

---
+++ 7_Bitwise_operator.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+// int x=10;
+
+// int fun(){
+// cout<<x<<endl;
+// }
+
+int main(){
+
+    // int a=4,b=8;
+    // cout<<(a&b)<<endl; //Bitwise and (&)-operator
+    // cout<<(a|b)<<endl; //Bitwise or (|)-operator
+    // cout<<(a^b)<<endl; //Bitwise xor (^)-operator
+    // cout<<endl;
+
+    // int c=3, d=7;
+    // cout<<(c&d)<<endl; //Bitwise and (&)-operator
+    // cout<<(c|d)<<endl; //Bitwise or (|)-operator
+    // cout<<(c^d)<<endl;
+    // cout<<endl;
+
+    // //Left shift
+    // cout<<(4<<1)<<endl;
+    // cout<<(10<<2)<<endl;
+    // cout<<endl;
+
+    // //Right shift
+    // cout<<(10>>1)<<endl;
+    // cout<<(8>>2)<<endl;
+    // cout<<endl;
+
+    // cout<<(5-2*6)<<endl; 
+    // cout<<((5-2)*6)<<endl;
+    // cout<<(4*5%2)<<endl;
+    // cout<<endl;
+
+    // fun();
+    // cout<<x<<endl;
+
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(long long int)<<endl;
+
+    unsigned int x = -10;
+    cout<<x<<endl;
+    return 0;
+}
