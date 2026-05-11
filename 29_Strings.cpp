553da4d58aef949b11ababf015b009a153f8f8ed
Ravi kumar <ravi720rathore@gmail.com>
2026-05-11 18:39:11 +0530

    Add files via upload

---
+++ 29_Strings.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    // string str1;
+    // cout<<"Ente your name : ";
+    // getline(cin, str1);
+    // cout<<"Your name is : "<<str1<<endl;
+
+
+    // string str2="Apna college";
+
+    // for(int i=0; i<str2.length(); i++){
+    //     cout<<str2[i]<<" ";
+    // }
+    // cout<<endl;
+
+    
+    // string str3="Google.com";
+    // for(int i=0; i<str3.length(); i++){
+    //     cout<<str3[i]<<" ";
+    // }
+    // cout<<endl;
+
+    // string str4="Learning programming";
+    // for(char ch : str4){
+    //     cout<<ch<<" ";
+    // }
+    // cout<<endl;
+
+    // int arr[]={1,2,3,4,5};
+    // int st=0;
+    // int eend=4;
+    // while(st<eend){
+    //     swap(arr[st],arr[eend]);
+    //     st++;
+    //     eend--;
+    // }
+    // for(int i=0; i<5; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<endl;
+
+    // char str[]={'R','A','V','I'};
+    // int stt=0, endd=3;
+    // while(stt<endd){
+    //     swap(str[stt],str[endd]);
+    //     stt++;
+    //     endd--;
+    // }
+    // for(int i=0; i<4; i++){
+    //     cout<<str[i]<<" ";
+    // }
+    // cout<<endl;
+
+    string str="apna college";
+     reverse(str.begin(), str.end());
+     cout<<str<<endl;
+
+     string str1="racecar";
+     int st=0, end=str1.length()-1;
+     is
+
+    return 0;
+} 
