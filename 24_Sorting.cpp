970c6ee1d693715e02f6a4a5ff4e494069edb001
Ravi kumar <ravi720rathore@gmail.com>
2026-05-11 18:38:00 +0530

    Add files via upload

---
+++ 24_Sorting.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    bool isSwap=false;
+   for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                isSwap=true;
+            }
+        }
+        if(!isSwap){
+            return; 
+        }
+    } 
+}
+
+void selectionSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int smallesIndex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[smallesIndex]){
+                smallesIndex=j;
+            }
+        }
+        swap(arr[i],arr[smallesIndex]);
+    }
+}
+
+void insertionSort(int arr[], int n){
+
+    for(int i=0; i<n; i++){
+        int curr=arr[i];
+        int prev=i-1;
+
+        while(prev>=0 && arr[prev]>curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1]=curr;
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n=5;
+    int arr[]={4,1,5,2,3};
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
